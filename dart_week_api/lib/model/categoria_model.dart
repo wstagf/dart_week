0b675b61669c7905bbeedbd2dfc87b56a564a081
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-03-31 21:58:56 -0300

    Criado categoria model

---
+++ dart_week_api/lib/model/categoria_model.dart
@@ -0,0 +1,18 @@
+import '../dart_week_api.dart';
+
+class CategoriaModel extends ManagedObject<_CategoriaModel>
+    implements _CategoriaModel {}
+
+enum TipoCategoria { receita, despesa }
+
+@Table(name: "categoria")
+class _CategoriaModel {
+  @Column(primaryKey: true, autoincrement: true)
+  int id;
+
+  @Column()
+  String nome;
+
+  @Column()
+  TipoCategoria tipoCategoria;
+}
