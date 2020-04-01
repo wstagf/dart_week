523d6e53be68c3b04dce5b44bc9c3d9e761afa44
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-03-31 22:30:01 -0300

    script para criacao de categorias

---
+++ dart_week_api/categorias_inserts.sql
@@ -0,0 +1,43 @@
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Salário', 'receita');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Investimento', 'receita');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Aplicação', 'receita');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Contas Residenciais', 'despesa');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Educação', 'despesa');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Mercado', 'despesa');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Saúde', 'despesa');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Transporte', 'despesa');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Compras', 'despesa');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Bar / Restaurantes', 'despesa');
+	
+	INSERT INTO public.categoria(
+	id, nome, tipocategoria)
+	VALUES (DEFAULT, 'Cuidados Pessoais', 'despesa');
