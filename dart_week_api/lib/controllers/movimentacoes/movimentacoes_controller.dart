01d72bbf27813b1e432fffa84932526e1807c12e
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-04 11:47:44 -0300

    implementado controller para buscar todas movimentações do mes

--- dart_week_api/lib/controllers/movimentacoes/movimentacoes_controller.dart
+++ dart_week_api/lib/controllers/movimentacoes/movimentacoes_controller.dart
@@ -1,7 +1,31 @@
 import 'package:dart_week_api/dart_week_api.dart';
+import 'package:dart_week_api/model/usuario_model.dart';
+import 'package:dart_week_api/services/movimentacoes_service.dart';
+import 'package:intl/intl.dart';
 
 class MovimentacoesController extends ResourceController {
-  MovimentacoesController(this.context);
+  MovimentacoesController(this.context)
+      : service = MovimentacoesService(context);
 
   final ManagedContext context;
+  final MovimentacoesService service;
+
+  @Operation.get('anoMes')
+  Future<Response> buscarTodasMovimentacoes() {
+    final anoMes = request.path.variables['anoMes'];
+    final DateFormat dateFormat = DateFormat('yyy-MM-dd');
+    final UsuarioModel usuario = request.attachments['usuario'] as UsuarioModel;
+
+    return service.buscarMovimentacoes(usuario, anoMes).then((data) {
+      return data
+          .map((m) => {
+                'id': m.id,
+                'dataMovimentacao': dateFormat.format(m.dataMovimentacao),
+                'descricao': m.descricao,
+                'valor': m.valor,
+                'categoria': {'id': m.categoria.id, 'nome': m.categoria.nome}
+              })
+          .toList();
+    }).then((lista) => Response.ok(lista));
+  }
 }
