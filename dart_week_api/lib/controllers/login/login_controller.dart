cbee9d3ce1b7b560032026da56cdf7da016f0bee
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-03 23:01:21 -0300

    validando o request

--- dart_week_api/lib/controllers/login/login_controller.dart
+++ dart_week_api/lib/controllers/login/login_controller.dart
@@ -11,6 +11,11 @@ class LoginController extends ResourceController {
 
   @Operation.post()
   Future<Response> login(@Bind.body() LoginRequest request) async {
+    final validate = request.validade();
+
+    if (validate.isNotEmpty) {
+      return Response.badRequest(body: validate);
+    }
     final token = await usuarioService.login(request);
     return Response.ok({'autenticado': token != null, 'token': token});
   }
