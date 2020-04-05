2e6163321c0ebebbc139d50527eef61378bb9f46
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-05 19:08:31 -0300

    criado componente textformfield

---
+++ minhaConta/lib/app/components/controlejaTextFormField.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ControlejaTextFormField extends TextFormField {
+  ControlejaTextFormField(
+      {ValueChanged<String> onChanged,
+      FormFieldValidator<String> validator,
+      @required String label,
+      bool obscureText,
+      double borderRadius,
+      double fontSize,
+      EdgeInsets padding,
+      TextEditingController controller})
+      : super(
+          controller: controller,
+          onChanged: onChanged,
+          validator: validator,
+          obscureText: obscureText ?? false,
+          decoration: InputDecoration(
+            contentPadding: padding ?? EdgeInsets.fromLTRB(30, 20, 0, 20),
+            labelText: label,
+            labelStyle: TextStyle(fontSize: fontSize ?? 20),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(borderRadius),
+            ),
+          ),
+        );
+}
