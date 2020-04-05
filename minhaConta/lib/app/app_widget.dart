8ac1d8f24119e36e54c85feef9f3381905e2e976
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-04 21:38:05 -0300

    Configurado OK Toast

--- minhaConta/lib/app/app_widget.dart
+++ minhaConta/lib/app/app_widget.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:oktoast/oktoast.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Slidy',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return OKToast(
+      position: ToastPosition.bottom,
+      child: MaterialApp(
+        title: 'Minhas Contas',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        onGenerateRoute: Modular.generateRoute,
       ),
-      initialRoute: '/',
-      onGenerateRoute: Modular.generateRoute,
     );
   }
 }
