543c53bfbcb2d16c6d020e4d0300877137510a8f
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-06 20:06:30 -0300

    criado componente painel saldo

--- minhaConta/lib/app/modules/movimentacoes/components/painel_saldo/painel_saldo_widget.dart
+++ minhaConta/lib/app/modules/movimentacoes/components/painel_saldo/painel_saldo_widget.dart
@@ -1,10 +1,172 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:minhaConta/app/modules/movimentacoes/components/painel_saldo/painel_saldo_controller.dart';
+import 'package:minhaConta/app/utils/size_utils.dart';
+import 'package:sliding_sheet/sliding_sheet.dart';
 
-class PainelSaldoWidget extends StatelessWidget {
+class PainelSaldoWidget extends StatefulWidget {
+  final double appbarHeight;
+
+  const PainelSaldoWidget({Key key, this.appbarHeight}) : super(key: key);
+  @override
+  _PainelSaldoWidgetState createState() => _PainelSaldoWidgetState();
+}
+
+class _PainelSaldoWidgetState
+    extends ModularState<PainelSaldoWidget, PainelSaldoController> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("textFinder"),
+    return SlidingSheet(
+      snapSpec: SnapSpec(
+        snap: true,
+        snappings: [0.1, 0.4],
+        positioning: SnapPositioning.relativeToAvailableSpace,
+      ),
+      headerBuilder: (_, state) {
+        return Container(
+          height: 5,
+          width: 60,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(20),
+          ),
+        );
+      },
+      builder: (_, state) {
+        return _makeContent();
+      },
+    );
+  }
+
+  Widget _makeContent() {
+    var arrow_back_back_ios;
+    return Container(
+      width: SizeUtils.widthScreen,
+      height: (SizeUtils.heigthScreen * 0.4) - widget.appbarHeight,
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back_ios),
+                ),
+                Text(
+                  'Janeiro / 2020',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_forward_ios),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(),
+            ),
+            Column(
+              children: <Widget>[
+                Text('Saldo'),
+                SizedBox(
+                  height: SizeUtils.heigthScreen * 0.01,
+                ),
+                Text(
+                  'R\$ 3.000,00',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Color(0xFF4BCE97),
+                            foregroundColor: Colors.white,
+                            child: Icon(Icons.arrow_upward),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Receitas',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Color(0xFF4BCE97),
+                              ),
+                            ),
+                            Text(
+                              'R\$ 200,00',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Color(0xFF4BCE97),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white,
+                            child: Icon(Icons.arrow_downward),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Despesas',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.red,
+                              ),
+                            ),
+                            Text(
+                              'R\$ 200,00',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- minhaConta/lib/app/modules/movimentacoes/movimentacoes_page.dart
+++ minhaConta/lib/app/modules/movimentacoes/movimentacoes_page.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:minhaConta/app/modules/movimentacoes/components/movimentacao_item.dart';
+import 'package:minhaConta/app/modules/movimentacoes/components/painel_saldo/painel_saldo_widget.dart';
 import 'package:minhaConta/app/utils/size_utils.dart';
 
 class MovimentacoesPage extends StatefulWidget {
-  final String title;
-  const MovimentacoesPage({Key key, this.title = "Movimentacoes"})
-      : super(key: key);
+  const MovimentacoesPage({Key key}) : super(key: key);
 
   @override
   _MovimentacoesPageState createState() => _MovimentacoesPageState();
 }
 
 class _MovimentacoesPageState extends State<MovimentacoesPage> {
+  AppBar appBar = AppBar(
+    title: Text('Movimentações'),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: appBar,
       body: Container(
         height: SizeUtils.heigthScreen,
         child: Stack(
           children: <Widget>[
             _makeContent(),
+            PainelSaldoWidget(
+              appbarHeight: appBar.preferredSize.height,
+            ),
           ],
         ),
       ),
