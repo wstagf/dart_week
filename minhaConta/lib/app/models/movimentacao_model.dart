dab921713dadabee86a0e2bf0cc86310a919e263
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-07 21:59:14 -0300

    criado movimentacoes model

---
+++ minhaConta/lib/app/models/movimentacao_model.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:minhaConta/app/models/categoria_model.dart';
+
+class MovimentacaoModel {
+  int id;
+  DateTime dataMovimentacao;
+  String descricao;
+  double valor;
+  CategoriaModel categoria;
+  MovimentacaoModel({
+    this.id,
+    this.dataMovimentacao,
+    this.descricao,
+    this.valor,
+    this.categoria,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'dataMovimentacao': dataMovimentacao.millisecondsSinceEpoch,
+      'descricao': descricao,
+      'valor': valor,
+      'categoria': categoria.toMap(),
+    };
+  }
+
+  static MovimentacaoModel fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return MovimentacaoModel(
+      id: map['id'],
+      dataMovimentacao:
+          DateTime.fromMillisecondsSinceEpoch(map['dataMovimentacao']),
+      descricao: map['descricao'],
+      valor: map['valor'],
+      categoria: CategoriaModel.fromMap(map['categoria']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  static MovimentacaoModel fromJson(String source) =>
+      fromMap(json.decode(source));
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is MovimentacaoModel &&
+        o.id == id &&
+        o.dataMovimentacao == dataMovimentacao &&
+        o.descricao == descricao &&
+        o.valor == valor &&
+        o.categoria == categoria;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        dataMovimentacao.hashCode ^
+        descricao.hashCode ^
+        valor.hashCode ^
+        categoria.hashCode;
+  }
+}
