20d9ddfcb047ff36583fa1d824f196d40a40f295
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-08 21:12:45 -0300

    criado usuario repository e funcao para login

---
+++ minhaConta/lib/app/repositories/usuario_repository.dart
@@ -0,0 +1,20 @@
+import 'package:minhaConta/app/core/custom_dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UsuarioRepository {
+  Future<bool> login(String login, String senha) {
+    final dio = CustomDio().instance;
+
+    return dio
+        .post('/login', data: {login: login, senha: senha}).then((res) async {
+      final String token = res.data['token'];
+      if (token != null) {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setString('token', token);
+        return true;
+      }
+
+      return false;
+    });
+  }
+}
