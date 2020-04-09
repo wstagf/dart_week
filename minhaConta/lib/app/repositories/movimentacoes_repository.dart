b3a7bd66c120eaa5799114cecb60777060fe44ba
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-08 21:31:43 -0300

    funcao getTotalMes

--- minhaConta/lib/app/repositories/movimentacoes_repository.dart
+++ minhaConta/lib/app/repositories/movimentacoes_repository.dart
@@ -1,5 +1,6 @@
 import 'package:minhaConta/app/core/custom_dio.dart';
 import 'package:minhaConta/app/models/movimentacao_model.dart';
+import 'package:minhaConta/app/models/movimentacao_total_model.dart';
 
 class MovimentacoesRepository {
   Future<List<MovimentacaoModel>> getMovimentacoes(String anoMes) {
@@ -7,5 +8,12 @@ class MovimentacoesRepository {
     return dio.get('/movimentacoes/$anoMes').then((res) => res.data
         .map<MovimentacaoModel>((c) => MovimentacaoModel.fromMap(c))
         .toList());
+  }
+
+  Future<MovimentacaoTotalModel> getTotalMes(String anoMes) {
+    final dio = CustomDio.whithAutentication().instance;
+    return dio
+        .get('/movimentacoes/total/$anoMes')
+        .then((res) => MovimentacaoTotalModel.fromMap(res.data));
   }
 }
