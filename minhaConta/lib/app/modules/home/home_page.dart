ca369f61c060ab476d5c399432ed8dd16cda97a5
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-12 11:26:10 -0300

    retirado regra de negocio da page para colocar no controller

--- minhaConta/lib/app/modules/home/home_page.dart
+++ minhaConta/lib/app/modules/home/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:get/get.dart';
-import 'package:minhaConta/app/repositories/usuario_repository.dart';
+import 'package:minhaConta/app/modules/home/home_controller.dart';
 import 'package:minhaConta/app/utils/size_utils.dart';
 import 'package:minhaConta/app/utils/theme_utils.dart';
 
@@ -13,19 +12,11 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     ThemeUtils.init(context);
     SizeUtils.init(context);
-
-    Future.delayed(Duration(seconds: 2), () async {
-      if (await Modular.get<UsuarioRepository>().isLogged()) {
-        Get.offAllNamed('/movimentacoes');
-      } else {
-        Get.offAllNamed('/login');
-      }
-    });
 
     return Scaffold(
       backgroundColor: ThemeUtils.primaryColor,
