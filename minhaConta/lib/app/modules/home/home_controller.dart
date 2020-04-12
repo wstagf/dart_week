e0e7938a40b78d741d78c549021f2eac9f9a0ae5
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-12 11:28:05 -0300

    implementado no controller

--- minhaConta/lib/app/modules/home/home_controller.dart
+++ minhaConta/lib/app/modules/home/home_controller.dart
@@ -1,15 +1,25 @@
+import 'package:get/get.dart';
+import 'package:minhaConta/app/mixins/loader_mixin.dart';
+import 'package:minhaConta/app/repositories/usuario_repository.dart';
 import 'package:mobx/mobx.dart';
 
 part 'home_controller.g.dart';
 
 class HomeController = _HomeBase with _$HomeController;
 
-abstract class _HomeBase with Store {
-  @observable
-  int value = 0;
+abstract class _HomeBase with Store, LoaderMixin {
+  final UsuarioRepository _usuarioRepository;
 
-  @action
-  void increment() {
-    value++;
+  _HomeBase(this._usuarioRepository);
+
+  Future<void> verificaLogado() async {
+    showLoader();
+    if (await _usuarioRepository.isLogged()) {
+      hideLoader();
+      Get.offAllNamed('/movimentacoes');
+    } else {
+      hideLoader();
+      Get.offAllNamed('/login');
+    }
   }
 }
--- minhaConta/lib/app/modules/home/home_module.dart
+++ minhaConta/lib/app/modules/home/home_module.dart
@@ -1,11 +1,12 @@
 import 'package:minhaConta/app/modules/home/home_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:minhaConta/app/modules/home/home_page.dart';
+import 'package:minhaConta/app/repositories/usuario_repository.dart';
 
 class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => HomeController()),
+        Bind((i) => HomeController(i.get<UsuarioRepository>())),
       ];
 
   @override
