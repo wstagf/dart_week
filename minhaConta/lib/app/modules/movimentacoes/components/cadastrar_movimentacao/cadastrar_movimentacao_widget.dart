055697357214cb529969dd04f69a4abf3c199f32
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-18 22:26:46 -0300

    retirado import

--- minhaConta/lib/app/modules/movimentacoes/components/cadastrar_movimentacao/cadastrar_movimentacao_widget.dart
+++ minhaConta/lib/app/modules/movimentacoes/components/cadastrar_movimentacao/cadastrar_movimentacao_widget.dart
@@ -2,7 +2,6 @@ import 'package:combos/combos.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:minhaConta/app/components/controleja_text_form_field.dart';
 import 'package:minhaConta/app/modules/movimentacoes/components/cadastrar_movimentacao/cadastrar_movimentacao_controller.dart';
