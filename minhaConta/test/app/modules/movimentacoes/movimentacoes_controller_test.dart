73dfb44cc5ddb24f3d7e0464be40692cd5b89da6
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-19 23:26:26 -0300

    implementado tests do movimentacao controller

--- minhaConta/test/app/modules/movimentacoes/movimentacoes_controller_test.dart
+++ minhaConta/test/app/modules/movimentacoes/movimentacoes_controller_test.dart
@@ -1,15 +1,39 @@
+import 'dart:js_util';
+
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:minhaConta/app/models/movimentacao_model.dart';
 import 'package:minhaConta/app/modules/movimentacoes/movimentacoes_controller.dart';
 import 'package:minhaConta/app/modules/movimentacoes/movimentacoes_module.dart';
+import 'package:mobx/mobx.dart';
 
 void main() {
   initModule(MovimentacoesModule());
-  MovimentacoesController movimentacoes;
+  MovimentacoesController controller;
 
   setUp(() {
-    movimentacoes = MovimentacoesModule.to.get<MovimentacoesController>();
+    controller = MovimentacoesModule.to.get<MovimentacoesController>();
   });
 
-  group('MovimentacoesController Test', () {});
+  group('MovimentacoesController Test', () {
+    test("Instanciou o controller", () {
+      expect(controller, isInstanceOf<MovimentacoesController>());
+    });
+
+    test("Existe campo errorMessage", () {
+      expect(controller.errorMessage, equals(null));
+    });
+
+    test("Existe campo movimentacoes", () {
+      // TODO Implementar o teste, antes, descobrir como faz teste de infanceOF ObservableList<MovimentacaoModel>
+    });
+
+    test("Existe campo _movimentacoesFuture", () {
+      // TODO Implementar o teste, antes, descobrir como faz teste de infanceOF ObservableFuture<List<MovimentacaoModel>>
+    });
+
+    test("Validar buscarMovimentacoes", () {
+      // TODO Implementar o teste, antes, descobrir como faz
+    });
+  });
 }
