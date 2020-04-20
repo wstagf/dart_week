c6a7150ee4585bb13368192da526832cb5184fe8
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-19 23:44:30 -0300

    comecando test do cadastrar movimentacao

--- minhaConta/test/app/modules/movimentacoes/components/cadastrar_movimentacao/cadastrar_movimentacao_controller_test.dart
+++ minhaConta/test/app/modules/movimentacoes/components/cadastrar_movimentacao/cadastrar_movimentacao_controller_test.dart
@@ -1,16 +1,24 @@
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:minhaConta/app/models/categoria_model.dart';
 import 'package:minhaConta/app/modules/movimentacoes/components/cadastrar_movimentacao/cadastrar_movimentacao_controller.dart';
 import 'package:minhaConta/app/modules/movimentacoes/movimentacoes_module.dart';
 
 void main() {
   initModule(MovimentacoesModule());
-  CadastrarMovimentacaoController cadastrarmovimentacao;
+  CadastrarMovimentacaoController controller;
 
   setUp(() {
-    cadastrarmovimentacao =
-        MovimentacoesModule.to.get<CadastrarMovimentacaoController>();
+    controller = MovimentacoesModule.to.get<CadastrarMovimentacaoController>();
   });
 
-  group('CadastrarMovimentacaoController Test', () {});
+  group('CadastrarMovimentacaoController Test', () {
+    test("Instanciou o controller", () {
+      expect(controller, isInstanceOf<CadastrarMovimentacaoController>());
+    });
+
+    test("Existe campo dataInclusao", () {
+      expect(controller.dataInclusao, isInstanceOf<DateTime>());
+    });
+  });
 }
