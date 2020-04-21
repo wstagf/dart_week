7f3ad512d0f70e1be6234edbeb3f85fa6e565c61
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-21 20:40:35 -0300

    configurando ozzie para fazer print dos testes

--- minhaConta/test_driver/app_test.dart
+++ minhaConta/test_driver/app_test.dart
@@ -1,5 +1,6 @@
 // Imports the Flutter Driver API.
 import 'package:flutter_driver/flutter_driver.dart';
+import 'package:ozzie/ozzie.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -9,15 +10,17 @@ void main() {
   final cadastreButtonFinder = find.byValueKey('cadastre_button');
 
   FlutterDriver driver;
+  Ozzie ozzie;
 
   Future<void> delay([int milliseconds = 250]) async {
     await Future<void>.delayed(Duration(milliseconds: milliseconds));
   }
 
-  group('Efetuar Login', () {
+  group('EfetuarLogin', () {
     // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
       driver = await FlutterDriver.connect();
+      ozzie = Ozzie.initWith(driver, groupName: 'EfetuarLogin');
     });
 
     // Close the connection to the driver after the tests have completed.
@@ -25,6 +28,17 @@ void main() {
       if (driver != null) {
         driver.close();
       }
+      ozzie.generateHtmlReport();
+    });
+
+    test('Teste print Login', () async {
+      await ozzie.profilePerformance('EfetuarLogin0', () async {
+        await driver.tap(loginInputFinder);
+        await ozzie.takeScreenshot('EfetuarLogin001');
+        await driver.enterText('thiago');
+        await ozzie.takeScreenshot('EfetuarLogin002');
+        await driver.waitFor(find.text('thiago'));
+      });
     });
 
     test('Preencher Login', () async {
