14326022b7ec099b8e7b68f803c537ac64014cda
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-25 17:08:11 -0300

    efective_dart

--- minhaConta/lib/app/repositories/categorias_repository.dart
+++ minhaConta/lib/app/repositories/categorias_repository.dart
@@ -5,8 +5,7 @@ import 'interfaces/categorias_repository_interface.dart';
 class CategoriaRepository implements ICategoriasRepository {
   Future<List<CategoriaModel>> getCategorias(String tipo) {
     final dio = CustomDio.whithAutentication().instance;
-    return dio.get('/categorias/$tipo').then((res) => res.data
-        .map<CategoriaModel>((c) => CategoriaModel.fromMap(c))
-        .toList());
+    return dio.get('/categorias/$tipo').then(
+        (res) => res.data.map<CategoriaModel>(CategoriaModel.fromMap).toList());
   }
 }
