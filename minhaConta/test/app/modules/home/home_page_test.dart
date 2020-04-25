d966603de0e9bab10165d54b8e6b10b3f8eb5bcf
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-24 21:53:11 -0300

    Implementado primeiro test de widget

--- minhaConta/test/app/modules/home/home_page_test.dart
+++ minhaConta/test/app/modules/home/home_page_test.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:minhaConta/app/app_module.dart';
+import 'package:minhaConta/app/modules/home/home_module.dart';
 
-import 'package:minhaConta/app/modules/home/home_page.dart';
+void main() {
+  setUp(() {
+    initModules([AppModule(), HomeModule()]);
+  });
 
-main() {
-  // testWidgets('HomePage has title', (WidgetTester tester) async {
-  //   await tester.pumpWidget(buildTestableWidget(HomePage(title: 'Home')));
-  //   final titleFinder = find.text('Home');
-  //   expect(titleFinder, findsOneWidget);
-  // });
+  testWidgets('Existe o loading', (tester) async {
+    final childWidget = Center(child: CircularProgressIndicator());
+
+    await tester.pumpWidget(Container(child: childWidget));
+
+    expect(find.byWidget(childWidget), findsOneWidget);
+  });
 }
