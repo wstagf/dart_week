dfeef78e553fc45c52ff8159efcecf42b7d774e3
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-04-26 11:22:27 -0300

    test da homepage implementado

--- minhaConta/test/app/modules/home/home_page_test.dart
+++ minhaConta/test/app/modules/home/home_page_test.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:minhaConta/app/app_controller.dart';
 import 'package:minhaConta/app/app_module.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:minhaConta/app/modules/home/home_module.dart';
+import 'package:minhaConta/app/modules/home/home_page.dart';
+import 'package:minhaConta/app/repositories/interfaces/usuario_repository_interface.dart';
+import 'package:minhaConta/app/repositories/mocks/usuario_repository_mock.dart';
 
 void main() {
+  initModule(AppModule());
+  initModule(HomeModule(), changeBinds: [
+    Bind<IUsuarioRepository>((i) => UsuarioRepositoryMock()),
+  ]);
+  AppController app;
+
   setUp(() {
-    initModules([AppModule(), HomeModule()]);
+    app = AppModule.to.get<AppController>();
   });
 
-  testWidgets('Existe o loading', (tester) async {
-    final childWidget = Center(child: CircularProgressIndicator());
+  testWidgets('Existe o loagind', (tester) async {
+    await tester.pumpWidget(buildTestableWidget(HomePage()));
 
-    await tester.pumpWidget(Container(child: childWidget));
+    final loadingFinder = find.byKey(Key('loading'));
 
-    expect(find.byWidget(childWidget), findsOneWidget);
+    expect(loadingFinder, findsOneWidget);
   });
 }
